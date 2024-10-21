5d674fe842c729b8cd608720523a68ebacdf342c
Shi Chen <shi@bscotch.net>
2024-10-21 10:51:52 -0500

    chore: Updated dependencies and the test file.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,21 +1,22 @@
 import {readFileSync} from 'fs'
 import {updateTestFlight} from '../src/app-store-connect-api'
 import {test} from '@jest/globals'
+import dotenv from 'dotenv'
+dotenv.config()
 
 test('Test updating AppStore Connect', async () => {
-  const appId = '123456789'
-  const version = '100.100.2017'
-  const group = 'external'
-
-  const secret = readFileSync('secret.json', 'utf8')
+  const appId = process.env.APPID || '123456789'
+  const version = process.env.VERSION ||'100.100.2017'
+  const group = process.env.GROUP ||'external'
+  const secret = process.env.SECRET as string
   const macConfig = JSON.parse(secret)
 
   await updateTestFlight(
     appId,
     version,
     group,
-    macConfig.iss,
-    macConfig.kid,
+    macConfig.issuer_id,
+    macConfig.key_id,
     macConfig.key,
     'desc'
   )
